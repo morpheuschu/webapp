69a8a00281bc9a8a3d2669623589f27389c68d17
morpheuschu <morpheus.chu@gmail.com>
2023-05-05 20:49:26 +0800

    new

--- app.py
+++ app.py
@@ -5,5 +5,22 @@
 # date : 2023-1/5/2023-{16:51}
 # project : webapp
 # file : app
+
+from flask import Flask, url_for, render_template, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+
+@app.route("/test")
+def test():
+    return redirect(url_for("index"))
+
+
+
 if __name__ == "__main__":
-    print("hello world")
+    app.run(debug=True, host="0.0.0.0", port=5000)
